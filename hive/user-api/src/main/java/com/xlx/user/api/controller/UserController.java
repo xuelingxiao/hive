ca96fbc6b58d64b0616ea55c5e354d5776d2020d
xuelingxiao <xuelinxiao@163.com>
2019-04-17 17:33:37 +0800

    add some model

--- hive/user-api/src/main/java/com/xlx/user/api/controller/UserController.java
+++ hive/user-api/src/main/java/com/xlx/user/api/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.xlx.user.api.controller;
 
-public class User {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
